2f3b270b8dc58a8744bbe0efd44ce267c21abd7a
Honore IRADUKUNDA <ihonore01@gmail.com>
2023-04-23 23:55:50 +0200

    added allowing db type

--- src/controllers/databaseController.js
+++ src/controllers/databaseController.js
@@ -3,32 +3,43 @@ import mysql from 'mysql';
 export class DatabaseController {
   establishDbConnection(req, res) {
 
-    const { host, user, password, database, query } = req.body;
+    let { host, user, password, database, query, dbType } = req.body;
 
-    const connection = mysql.createConnection({
-      host,
-      user,
-      password,
-      database,
-    });
+    if (dbType) dbType = dbType.toLowerCase();
 
-    connection.connect((err) => {
-      if (err) {
-        console.error('Error connecting to database:', err);
-        res.status(500).json({ error: 'Error connecting to database' });
-        return;
-      }
+    if (dbType == 'mysql' || dbType == 'mariadb' || !dbType) {
 
-      connection.query(query, (err, results) => {
+      const connection = mysql.createConnection({
+        host,
+        user,
+        password,
+        database,
+      });
+
+      connection.connect((err) => {
         if (err) {
-          console.error('Error executing query:', err);
-          res.status(500).json({ error: 'Error executing query, Please make sure your SQL query is Valid' });
+          console.error('Error connecting to database:', err);
+          res.status(500).json({ error: 'Error connecting to database' });
           return;
         }
 
-        res.json({ results });
+        connection.query(query, (err, results) => {
+          if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).json({ error: 'Error executing query, Please make sure your SQL query is Valid' });
+            return;
+          }
+
+          res.json({ results });
+        });
       });
-    });
+    } else if (dbType == 'postgresql') {
+      //TO DO
+      res.status(500).json({ error: 'PostegreSQL' });
+    } else {
+      res.status(500).json({ error: 'Database not supported, MySQL,MariaDB and PostgreSQL are currently supported DBs' });
+    }
+
 
   }
 }
